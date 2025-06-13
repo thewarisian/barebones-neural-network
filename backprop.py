638b96e0bd1fef7b61dbd6f74a2011041de2e917
Ayaan Waris <wayaan729@gmail.com>
2025-06-13 12:22:40 +0530

    Added one loss function and activation function each.

--- backprop.py
+++ backprop.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+#Activation functions
+tanh = lambda x: np.tanh(x)
+tanh_prime = lambda x: 1 - np.tanh(x)**2
+
+#Gradient descent
 def grad_descent(input, output, weight, bias, out_grad, learn_rate, activation, activation_prime):
     in_grad = np.dot(weight.T, out_grad)
     if activation is not None:
@@ -9,3 +14,9 @@ def grad_descent(input, output, weight, bias, out_grad, learn_rate, activation, 
     bias -= learn_rate * out_grad
 
     return in_grad, weight, bias
+
+#Loss functions
+def mse(nn_output, correct_output):
+    return np.mean(np.power((correct_output - nn_output), 2))
+def mse_prime(nn_output, correct_output):
+    return 2*(nn_output - correct_output) / np.size(correct_output)
